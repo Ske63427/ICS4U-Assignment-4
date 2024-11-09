b7e2b7255d6648f072e580bc98528f6af22041b2
email <skelecrae63427@gmail.com>
2024-11-09 09:41:49 -0500

    successfully turned heronsFormula and ambiguousCase into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,25 @@
 import "./App.css";
 import { useState } from 'react';
+import HeronsFormula from './components/heronsFormula.jsx';
+import AmbiguousCase from './components/ambiguousCase.jsx';
 
 function App() {
-  const [hfa, sethfA] = useState(0)
-  const [hfb, sethfB] = useState(0)
-  const [hfc, sethfC] = useState(0)
-  const [hfArea, setHfArea] = useState("Press Calulate")
-
-  function heronsFormula(e){
-    e.preventDefault();
-    setHfArea(0.25 * Math.sqrt(4 * hfa ** 2 * hfb ** 2 - (hfa ** 2 + hfb ** 2 - hfc ** 2) ** 2))
-  }
-
-  let triangle
-  const [aca, setacA] = useState(0)
-  const [acb, setacB] = useState(0)
-  const [acAA, setacAA] = useState(0)
-  const [acCase, setacCase] = useState("Press Calulate")
-
-  function ambiguousCase(e){
-    e.preventDefault();
-    let h = Math.abs(Math.round(acb * Math.sin((acAA * Math.PI) / 180)))
-    /** Test Cases: 
-     * 15, 15, 30; 21, 14, 115: one triangle
-     * 15, 30, 40; 11, 15, 125: no triangles
-     * 15, 23, 30: two triangles
-     * 15, 30, 30: right triangle
-     */
-    if (acAA < 90) {
-      if (aca > h && aca < acb) triangle = "Two Triangles"
-      else if (aca == h) triangle = "Right Triangle"
-      else if (aca < h) triangle = "No Triangle"
-      else triangle = "One Triangle"
-    } else {
-      if (aca < acb || aca == acb) triangle = "No Triangles"
-      else if (aca > acb) triangle = "One Triangle"
-    }
-    setacCase(triangle)
-  }
 
   const [coeffs, setCoeffs] = useState(0)
   const [exps, setExps] = useState(0)
   const [xValue, setXValue] = useState(0)
   const [polyDisp, setPolyDisp] = useState(0)
   const [polyEval, setPolyEval] = useState(0)
-  
+
   function getPolynomial(e){
-    setPolyDisp
+    //setPolyDisp
   }
 
   return (
     <div className="parent">
-      <div className="heronsFormula">
-        <form onSubmit={(e) => heronsFormula(e)}>
-          <h3>Heron's Formula</h3>
-          <label>a:</label><br/>
-          <input id="hfA" type="number" value={hfa} onChange={(event) => {sethfA(event.target.value)}} autoComplete="off" /><br/><br/>
-
-          <label>b:</label><br/>
-          <input id="hfB" type="number" value={hfb} onChange={(event) => {sethfB(event.target.value)}} autoComplete="off" /><br/><br/>
-
-          <label>c:</label><br/>
-          <input id="hfC" type="number" value={hfc} onChange={(event) => {sethfC(event.target.value)}} autoComplete="off" /><br/><br/>
-
-          <label>Area:</label><br/>
-          <input id="hfAnswerBlock" value={hfArea} type="text" disabled autoComplete="off"/><br/><br/>
-          <button id="hfSubmit">Calculate</button>
-        </form>
-      </div>
-
-      <br />
-
-      <div className="ambiguousCase">
-        <form onSubmit={(e) => ambiguousCase(e)}>
-        <h3>Ambiguous Case</h3>
-          <label>a:</label><br/>
-          <input id="acA" type="number" value={aca} onChange={(event) => {setacA(event.target.value)}} autoComplete="off"/><br/><br/>
-
-          <label>b:</label><br/>
-          <input id="acB" type="number" value={acb} onChange={(event) => {setacB(event.target.value)}} autoComplete="off"/><br/><br/>
-
-          <label>Angle A:</label><br/>
-          <input id="acAngleA" type="number" value={acAA} onChange={(event) => {setacAA(event.target.value)}} autoComplete="off"/><br/><br/>
-
-          <input id="acAnswerBlock" value={acCase} type="text" disabled autoComplete="off"/><br/><br/>
-          <button id="acSubmit">Calculate</button>
-        </form>
-      </div>
-
+      <HeronsFormula />
+      <br/>
+      <AmbiguousCase />
       <br/>
 
       <div className="newtonsMethod">
---
+++ src/components/heronsFormula.jsx
@@ -0,0 +1,42 @@
+import "./heronsFormula.css";
+import { useState } from 'react';
+
+function heronsFormula(){
+    const [hfa, sethfA] = useState(0)
+    const [hfb, sethfB] = useState(0)
+    const [hfc, sethfC] = useState(0)
+    const [hfArea, setHfArea] = useState("Press Calculate")
+
+    function calculateArea(e){
+        e.preventDefault();
+        setHfArea(0.25 * Math.sqrt(4 * hfa ** 2 * hfb ** 2 - (hfa ** 2 + hfb ** 2 - hfc ** 2) ** 2))
+    }
+
+    return (
+        <div className="heronsFormula">
+            <form onSubmit={(e) => calculateArea(e)}>
+                <h3>Heron's Formula</h3>
+                <label>a:</label><br/>
+                <input id="hfA" type="number" value={hfa} onChange={(event) => {
+                    sethfA(event.target.value)
+                }} autoComplete="off"/><br/><br/>
+
+                <label>b:</label><br/>
+                <input id="hfB" type="number" value={hfb} onChange={(event) => {
+                    sethfB(event.target.value)
+                }} autoComplete="off"/><br/><br/>
+
+                <label>c:</label><br/>
+                <input id="hfC" type="number" value={hfc} onChange={(event) => {
+                    sethfC(event.target.value)
+                }} autoComplete="off"/><br/><br/>
+
+                <label>Area:</label><br/>
+                <input id="hfAnswerBlock" value={hfArea} type="text" disabled autoComplete="off"/><br/><br/>
+                <button id="hfSubmit">Calculate</button>
+            </form>
+        </div>
+    )
+}
+
+export default heronsFormula;
