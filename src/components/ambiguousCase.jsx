46d2002c27b2b83a3cecdf30e2f2c6d43203f279
Aidan Samaroo <aidan.samaroo@student.tdsb.on.ca>
2024-11-18 11:00:40 -0500

    post-grading changes

--- src/components/ambiguousCase.jsx
+++ src/components/ambiguousCase.jsx
@@ -1,48 +1,94 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function ambiguousCase() {
-    let triangle
-    const [aca, setacA] = useState(0)
-    const [acb, setacB] = useState(0)
-    const [acAA, setacAA] = useState(0)
-    const [acCase, setacCase] = useState("Press Calculate")
+  let triangle;
+  const [aca, setacA] = useState(0);
+  const [acb, setacB] = useState(0);
+  const [acAA, setacAA] = useState(0);
+  const [acCase, setacCase] = useState("Press Calculate");
 
-    function ambiguousCase(e) {
-        e.preventDefault()
-        let h = Math.abs(Math.round(acb * Math.sin((acAA * Math.PI) / 180)))
-        /** Test Cases:
-         * 15, 15, 30; 21, 14, 115: one triangle
-         * 15, 30, 40; 11, 15, 125: no triangles
-         * 15, 23, 30: two triangles
-         * 15, 30, 30: right triangle
-         */
-        if (acAA < 90) {
-            if (aca > h && aca < acb) triangle = "Two Triangles"
-            else if (aca == h) triangle = "Right Triangle"
-            else if (aca < h) triangle = "No Triangle"
-            else triangle = "One Triangle"
-        } else triangle = (aca <= acb) ? "No Triangles" : "One Triangle"
-        setacCase(triangle)
+  function ambiguousCase(e) {
+    e.preventDefault();
+    let h = Math.abs(Math.round(acb * Math.sin((acAA * Math.PI) / 180)));
+    /** Test Cases:
+     * 15, 15, 30; 21, 14, 115: one triangle
+     * 15, 30, 40; 11, 15, 125: no triangles
+     * 15, 23, 30: two triangles
+     * 15, 30, 30: right triangle
+     */
+    if (acAA < 90) {
+      if (aca > h && aca < acb) {
+        triangle = "Two Triangles";
+      } else if (aca == h) {
+        triangle = "Right Triangle";
+      } else if (aca < h) {
+        triangle = "No Triangle";
+      } else triangle = "One Triangle";
+    } else {
+      triangle = aca <= acb ? "No Triangles" : "One Triangle";
     }
+    setacCase(triangle);
+  }
 
-    return (
-        <div className="ambiguousCase">
-            <form onSubmit={(e) => ambiguousCase(e)}>
-                <h3>Ambiguous Case</h3>
-                <label>a:</label><br/>
-                <input id="acA" type="number" value={aca} onChange={(event) => {setacA(event.target.value)}} autoComplete="off"/><br/><br/>
+  return (
+    <div className="ambiguousCase">
+      <form onSubmit={(e) => ambiguousCase(e)}>
+        <h3>Ambiguous Case</h3>
+        <label>a:</label>
+        <br />
+        <input
+          id="acA"
+          type="number"
+          value={aca}
+          onChange={(event) => {
+            setacA(event.target.value);
+          }}
+          autoComplete="off"
+        />
+        <br />
+        <br />
 
-                <label>b:</label><br/>
-                <input id="acB" type="number" value={acb} onChange={(event) => {setacB(event.target.value)}} autoComplete="off"/><br/><br/>
+        <label>b:</label>
+        <br />
+        <input
+          id="acB"
+          type="number"
+          value={acb}
+          onChange={(event) => {
+            setacB(event.target.value);
+          }}
+          autoComplete="off"
+        />
+        <br />
+        <br />
 
-                <label>Angle A:</label><br/>
-                <input id="acAngleA" type="number" value={acAA} onChange={(event) => {setacAA(event.target.value)}} autoComplete="off"/><br/><br/>
+        <label>Angle A:</label>
+        <br />
+        <input
+          id="acAngleA"
+          type="number"
+          value={acAA}
+          onChange={(event) => {
+            setacAA(event.target.value);
+          }}
+          autoComplete="off"
+        />
+        <br />
+        <br />
 
-                <input id="acAnswerBlock" value={acCase} type="text" disabled autoComplete="off"/><br/><br/>
-                <button id="acSubmit">Calculate</button>
-            </form>
-        </div>
-    )
+        <input
+          id="acAnswerBlock"
+          value={acCase}
+          type="text"
+          disabled
+          autoComplete="off"
+        />
+        <br />
+        <br />
+        <button id="acSubmit">Calculate</button>
+      </form>
+    </div>
+  );
 }
 
-export default ambiguousCase
+export default ambiguousCase;
--- src/components/newtonsMethod.jsx
+++ src/components/newtonsMethod.jsx
@@ -1,71 +1,117 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function newtonsMethod() {
-    const [coeffs, setCoeffs] = useState("")
-    const [exps, setExps] = useState("")
-    const [rootGuess, setRootGuess] = useState(0)
-    let [rootApprox, setRootApprox] = useState(0)
+  const [coeffs, setCoeffs] = useState("");
+  const [exps, setExps] = useState("");
+  const [rootGuess, setRootGuess] = useState(0);
+  let [rootApprox, setRootApprox] = useState(0);
 
-    function getPolynomial(){
-        const coefficientArray = coeffs.split(" ").map(Number)
-        const exponentArray = exps.split(" ").map(Number)
-        return [[...coefficientArray], [...exponentArray]]
+  function getPolynomial() {
+    const coefficientArray = coeffs.split(" ").map(Number);
+    const exponentArray = exps.split(" ").map(Number);
+    return [[...coefficientArray], [...exponentArray]];
+  }
+
+  function evaluatePolynomial(values, x) {
+    let polyEval = 0;
+    for (let i = 0; i < values[0].length; i++) {
+      polyEval += values[0][i] * x ** values[1][i];
     }
+    return polyEval;
+  }
 
-    function evaluatePolynomial(values, x) {
-        let polyEval = 0
-        for (let i = 0; i < values[0].length; i++){
-            polyEval += values[0][i] * x ** values[1][i]
-        }
-        return polyEval
+  function evaluateDerivative(values, x) {
+    let derivativeEval = 0;
+    for (let i = 0; i < values[0].length; i++) {
+      if (values[1][i] !== 0) {
+        derivativeEval += values[1][i] * values[0][i] * x ** (values[1][i] - 1);
+      }
     }
+    return derivativeEval;
+  }
 
-    function evaluateDerivative(values, x){
-        let derivativeEval = 0
-        for (let i = 0; i < values[0].length; i++) {
-            if (values[1][i] !== 0) {
-                derivativeEval += values[1][i] * values[0][i] * x ** (values[1][i] - 1)
-            }
-        }
-        return derivativeEval
+  function approximate(x0, evaluation, derivative) {
+    return x0 - evaluation / derivative;
+  }
+
+  function newtonsMethod(e) {
+    e.preventDefault();
+    let x0 = rootGuess;
+    let rootApprox;
+
+    for (let i = 0; i < 100; i++) {
+      rootApprox = approximate(
+        x0,
+        evaluatePolynomial(getPolynomial(), x0),
+        evaluateDerivative(getPolynomial(), x0)
+      );
+      x0 = rootApprox;
     }
+    setRootApprox(x0);
+  }
 
-    function approximate(x0, evaluation, derivative){
-        return x0 - (evaluation / derivative)
-    }
+  return (
+    <div className="newtonsMethod">
+      <form onSubmit={(e) => newtonsMethod(e)}>
+        <h3>Newtons Method</h3>
+        <label>Coefficients:</label>
+        <br />
+        <input
+          id="pfC"
+          type="text"
+          value={coeffs}
+          onChange={(event) => {
+            setCoeffs(event.target.value);
+          }}
+          autoComplete="off"
+        />
+        <br />
+        <br />
 
-    function newtonsMethod(e){
-        e.preventDefault()
-        let x0 = rootGuess
-        let rootApprox
+        <label>Exponents:</label>
+        <br />
+        <input
+          id="pfE"
+          type="text"
+          value={exps}
+          onChange={(event) => {
+            setExps(event.target.value);
+          }}
+          autoComplete="off"
+        />
+        <br />
+        <br />
 
-        for (let i = 0; i < 25; i++){
-            rootApprox = approximate(x0, evaluatePolynomial(getPolynomial(), x0), evaluateDerivative(getPolynomial(), x0))
-            x0 = rootApprox
-        }
-        setRootApprox(x0)
-    }
+        <label>Root Guess:</label>
+        <br />
+        <input
+          id="nmG"
+          type="number"
+          value={rootGuess}
+          onChange={(event) => {
+            setRootGuess(event.target.value);
+          }}
+          autoComplete="off"
+        />
+        <br />
+        <br />
 
-    return (
-        <div className="newtonsMethod">
-            <form onSubmit={(e) => newtonsMethod(e)}>
-                <h3>Newtons Method</h3>
-                <label>Coefficients:</label><br/>
-                <input id="pfC" type="text" value={coeffs} onChange={(event) => {setCoeffs(event.target.value)}} autoComplete="off"/><br/><br/>
+        <label>Root Approximation:</label>
+        <br />
+        <input
+          id="nmAnswerBlock"
+          value={rootApprox}
+          type="text"
+          disabled
+          autoComplete="off"
+        />
+        <br />
+        <br />
 
-                <label>Exponents:</label><br/>
-                <input id="pfE" type="text" value={exps} onChange={(event) => {setExps(event.target.value)}} autoComplete="off"/><br/><br/>
-
-                <label>Root Guess:</label><br/>
-                <input id="nmG" type="number" value={rootGuess} onChange={(event) => {setRootGuess(event.target.value)}} autoComplete="off"/><br/><br/>
-
-                <label>Root Approximation:</label><br/>
-                <input id="nmAnswerBlock" value={rootApprox} type="text" disabled autoComplete="off"/><br/><br/>
-
-                <button id="nmSubmit">Calculate</button>
-            </form>
-        </div>
-    )
+        <button id="nmSubmit">Calculate</button>
+      </form>
+    </div>
+  );
 }
 
-export default newtonsMethod
+export default newtonsMethod;
